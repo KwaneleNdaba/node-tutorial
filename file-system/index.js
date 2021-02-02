1998ab7c76da66e581c80848fff2d4d36d3560f8
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2021-02-02 12:00:00 +0200

    initial

---
+++ file-system/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const dataFolder = path.join(__dirname, "data");
+
+if(!fs.existsSync(dataFolder)){
+    fs.mkdirSync(dataFolder);
+    console.log("Data folder created");
+}
+
+const filePath = path.join(dataFolder, "example.txt");
+
+fs.writeFileSync(filePath, "Hello from node js");
+
+const readContentFromFile = fs.readFileSync(filePath, "utf8")
+
+
+console.log("File Content: ", readContentFromFile);
+
+fs.appendFileSync(filePath, )
