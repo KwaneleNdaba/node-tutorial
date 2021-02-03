a524dc83a33310b53003c6208beb022d20152cb3
KwaneleNdaba <113377563+KwaneleNdaba@users.noreply.github.com>
2021-02-03 12:00:00 +0200

    file system complete

--- file-system/index.js
+++ file-system/index.js
@@ -19,3 +19,23 @@ console.log("File Content: ", readContentFromFile);
 
 fs.appendFileSync(filePath, "\nThis is a new line added to this file");
 console.log("New file content added");
+
+
+const asyncFilePath = path.join(dataFolder, "async-example.txt");
+fs.writeFile(asyncFilePath, "Hello Async node js", (err) => {
+    if(err) throw err;
+    console.log("Async file has been created successfully");
+    fs.readFile(asyncFilePath, "utf8", (error, data) => {
+        if(err) throw err;
+        console.log("Async file content:", data);
+
+        fs.appendFile(asyncFilePath, "\nThis is a new line for async file", (error) => {
+            if(error)throw error;
+            console.log("New line added to async file");
+        })
+        fs.readFile(asyncFilePath, "utf8", (error, udpatedData) => {
+            if(error) throw error;
+            console.log("Updated data: ", udpatedData)
+        });
+    })
+})
